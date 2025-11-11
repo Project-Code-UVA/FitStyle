4b00bd7bcf7bc9c95d04bfc5b985733cf4102d52
aziza50 <aziza.ergasheva50@gmail.com>
2025-11-10 23:07:00 -0500

    Added a webcam for capturing video

--- FitStyle/src/pages/Home.jsx
+++ FitStyle/src/pages/Home.jsx
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+//import model from "../../models/pose_landmarker_lite.task";
+import Webcam from "react-webcam";
+// import {
+//   PoseLandmarker,
+//   FilesetResolver,
+//   DrawingUtils,
+// } from "https://cdn.skypack.dev/@mediapipe/tasks-vision@0.10.0";
 
 function Home() {
-  return <div></div>;
+  const webCamRef = useRef(null);
+  const [webCamOn, setwebCamOn] = useState(false);
+  const handlewebCamOn = () => {
+    setwebCamOn(true);
+  };
+  // async function createVisionModel() {
+  //   try {
+  //     const vision = await FilesetResolver.forVisionTasks(
+  //       "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
+  //     );
+  //     const poseLandmarker = await poseLandmarker.createFromOptions(vision, {
+  //       baseOptions: {
+  //         modelAssetPath: model,
+  //         delegate: "GPU",
+  //       },
+  //       runningMode: runningMode,
+  //     });
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
+  // createVisionModel();
+
+  // async function handleVideo() {
+  //   await poseLandmarker.setOptions({ runningMode: "VIDEO" });
+  //   let lastVideoTime = -1;
+  //   function renderLoop() {
+  //     const video = document.getElementById("video");
+
+  //     if (video.currentTime !== lastVideoTime) {
+  //       const poseLandmarkerResult = poseLandmarker.detectForVideo(video);
+  //       prcessResults(detections);
+  //       lastVideoTime = video.currentTime;
+  //     }
+
+  //     requestAnimationFrame(() => {
+  //       renderLoop();
+  //     });
+  //   }
+  // }
+
+  // async function handleImage() {
+  //   await poseLandmarker.setOptions({ runningMode: "IMAGE" });
+  // }
+
+  return (
+    <>
+      <div className="mt-[50px] justify-center flex">
+        {webCamOn && (
+          <Webcam ref={webCamRef} videoConstraints={{ facingMode: "user" }} />
+        )}
+      </div>
+
+      <div className="flex mt-[50px] justify-self-center">
+        <button
+          onClick={handlewebCamOn}
+          className="text-center rounded-4xl bg-blue-200 w-[150px]"
+        >
+          Start Webcam
+        </button>
+
+        <button className="text-center rounded-4xl bg-blue-200 w-[150px]">
+          Upload an Image
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default Home;
